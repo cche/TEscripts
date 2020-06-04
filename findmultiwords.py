ed94bf5508d1a0d616cd6fe0e1762c3c4b79ef3a
Cristian <cche@users.noreply.github.com>
2020-06-04 18:23:33 +0200

    Working version
    Print out to file or stdout the size, words, positions and chrom start
    end for each position.

--- findmultiwords.py
+++ findmultiwords.py
@@ -1,68 +1,94 @@
 #!/usr/bin/env python3
 
 from collections import Counter
+import sys
+import contextlib
 
 
-class NgramList():
+@contextlib.contextmanager
+def smart_open(filename=None):
+    if filename and filename != "-":
+        fh = open(filename, "w")
+    else:
+        fh = sys.stdout
+
+    try:
+        yield fh
+    finally:
+        if fh is not sys.stdout:
+            fh.close()
+
+
+class NgramList:
     allwords = []
     covered = []
 
     def __init__(self, file):
         # TODO Use the index (line number) as key, this will allow the elimination
         # TODO of the overlapping profiles.
-        with open(args.i) as infile:
+        self.positions = []
+        with open(file) as infile:
             for line in infile:
-                word = line.split('\t')[10]
+                fields = line.strip().split("\t")
+                self.positions.append([fields[4], fields[5], fields[6]])
+                word = fields[9]
                 NgramList.allwords.append(word)
                 NgramList.covered.append(0)
 
         self.ngrams = {}
-        for start, word in enumerate(NgramList.allwords):
-            if word in self.ngrams:
-                self.ngrams[word].add_start(start)
+        #  for start, word in enumerate(NgramList.allwords):
+        for start, word in getNwords(NgramList.allwords, 1):
+            nameword = "_".join(word)
+            if nameword in self.ngrams:
+                self.ngrams[nameword].add_start(start)
             else:
-                self.ngrams[word] = Ngram(word, start)
+                self.ngrams[nameword] = Ngram(word, start)
+        #  print(self.ngrams, len(self.ngrams))
+        #  print("1--", len(self.ngrams))
 
-    def grow_ngrams(self):
+    def grow_ngrams(self, turns=0):
         ngs = list(self.ngrams.keys())
         more = True
-        #  i = 3
+        i = 0
         while len(ngs) and more:
             more = False
             for ng in ngs:
                 ngram = self.ngrams[ng]
                 #  print(ngram)
                 if ngram.get_next():
-                    # mark the positions in covered (list) so that we don't visit them again.
-                    #  print("found longer ngram")
                     more = True
                     #  for pos in ngram.start:
                     #      for n in range(ngram.nsize+1):
                     #          NgramList.covered[pos+n] = 1
                 else:
                     ngs.remove(ng)
-            #  print("ngs: ", len(ngs), more)
-            #  print(f"checked ngram {i}")
-            #  i += 1
+            i += 1
+            if i >= turns:
+                break
+        #  print("grow", len(self.ngrams))
 
     def clean_overlapping(self):
         ngs = list(self.ngrams.keys())
         todelete = []
         #  print(len(ngs))
         i = 0
-        while i <= len(ngs)-1:
+        while i <= len(ngs) - 1:
             #  print(f"doing {i}")
             try:
                 ng1 = self.ngrams[ngs[i]]
             except:
+                #  print(f"didn't find {ngs[i]}")
                 break
             if ng1.nsize < 2:
                 i += 1
                 continue
-            for j in range(i+1, len(ngs)):
+            for j in range(i + 1, len(ngs)):
                 ng2 = self.ngrams[ngs[j]]
                 #  print(ng1, ng2)
-                if are_overlapping([ng1.start[0], ng1.start[0]+ng1.nsize], [ng2.start[0], ng2.start[0]+ng2.nsize]):
+                if are_overlapping(
+                    [ng1.start[0], ng1.start[0] + ng1.nsize],
+                    [ng2.start[0], ng2.start[0] + ng2.nsize],
+                ):
                     #  print(f"overlapping with {j}")
                     diff = ng2.start[0] - ng1.start[0]
                     if diff > 0:
@@ -74,20 +100,81 @@ class NgramList():
                     break
             i = j
 
-    def printNgram(self, minsize=2):
-        for ng in self.ngrams:
-            if self.ngrams[ng].nsize >= minsize and len(self.ngrams[ng].start) >= 2:
-                print(self.ngrams[ng])
+    def stats(self):
+        maxlen = max([self.ngrams[n].nsize for n in self.ngrams])
+        print(f"number of ngrams: {len(self.ngrams)}")
+        print(f"longest ngram: {maxlen}")
+
+    def writeNgrams(self, file=None, minsize=2, maxsize=None):
+        with smart_open(file) as infile:
+            for ng in self.ngrams:
+                if len(self.ngrams[ng].start) < 2:
+                    continue
+                if self.ngrams[ng].nsize >= minsize:
+                    #  print(minsize, maxsize)
+                    if maxsize:
+                        if self.ngrams[ng].nsize <= maxsize:
+                            print(self.ngrams[ng], file=infile)
+                            for st in self.ngrams[ng].start:
+                                regstart = min(
+                                    [
+                                        p[1]
+                                        for p in self.positions[
+                                            st : st + self.ngrams[ng].nsize + 1
+                                        ]
+                                    ]
+                                )
+                                regstop = max(
+                                    [
+                                        p[2]
+                                        for p in self.positions[
+                                            st : st + self.ngrams[ng].nsize + 1
+                                        ]
+                                    ]
+                                )
+                                print(
+                                    "\t".join(
+                                        [self.positions[st][0], regstart, regstop]
+                                    ),
+                                    file=infile,
+                                )
+                    else:
+                        print(self.ngrams[ng], file=infile)
+                        for st in self.ngrams[ng].start:
+                            regstart = min(
+                                [
+                                    p[1]
+                                    for p in self.positions[
+                                        st : st + self.ngrams[ng].nsize + 1
+                                    ]
+                                ]
+                            )
+                            regstop = max(
+                                [
+                                    p[2]
+                                    for p in self.positions[
+                                        st : st + self.ngrams[ng].nsize + 1
+                                    ]
+                                ]
+                            )
+                            print(
+                                "\t".join([self.positions[st][0], regstart, regstop]),
+                                file=infile,
+                            )
 
 
 class Ngram(object):
     def __init__(self, words, start):
-        self.words = [words]
-        self.start = [start, ]
+        self.words = [
+            words,
+        ]
+        self.start = [
+            start,
+        ]
         self.nsize = len(self.words)
 
     def __repr__(self):
-        return f"{self.nsize}\n {self.words}\n {self.start}"
+        return f"#{str(self.nsize)}\t{self.words}\t{self.start}"
 
     def get_next(self):
         newwords = Counter()
@@ -96,18 +183,20 @@ class Ngram(object):
             if NgramList.covered[pos] == 1:
                 self.start.remove(pos)
                 continue
-            if pos+self.nsize >= len(NgramList.allwords):
+            if pos + self.nsize >= len(NgramList.allwords):
                 continue
             #  print(pos, self.nsize)
-            nw = NgramList.allwords[pos+self.nsize]
+            nw = NgramList.allwords[pos + self.nsize]
             #  print(nw, NgramList.allwords[pos+self.nsize])
             newwords[nw] += 1
             if nw not in newstarts:
                 newstarts[nw] = []
             newstarts[nw].append(pos)
 
-        if newwords.most_common(1) and \
-                100.0*newwords.most_common(1)[0][1]/len(self.start) >= 30:
+        if (
+            newwords.most_common(1)
+            and 100.0 * newwords.most_common(1)[0][1] / len(self.start) >= 30
+        ):
             #  print("perc cov", 100.0*newwords.most_common(1)[0][1]/len(self.start))
             for n in self.start:
                 #  print(newstarts[newwords.most_common(1)[0][0]])
@@ -126,35 +215,37 @@ class Ngram(object):
             self.nsize += 1
             self.words += [word]
             return True
-        for i in range(len(self.start)-1):
-            if self.start[i]+self.nsize+1 < self.start[i+1]:
+        for i in range(len(self.start) - 1):
+            if self.start[i] + self.nsize + 1 < self.start[i + 1]:
                 self.nsize += 1
                 self.words += [word]
                 return True
             else:
                 # we got a repeat element
-                del self.start[i+1]
+                del self.start[i + 1]
                 return False
 
 
-def get2words(words):
-    for i in range(len(words)-1):
-        yield [i, [words[i], words[i+1]]]
+def getNwords(words, n):
+    for i in range(len(words) - n):
+        yield [i, words[i : i + n]]
 
 
 def are_overlapping(r, s):
-    return not(r[1] < s[0] or s[1] < r[0])
+    return not (r[1] < s[0] or s[1] < r[0])
 
 
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(
-        description="Find all repeated word sequences in a column")
+        description="Find all repeated word sequences in a column"
+    )
     parser.add_argument("-i", help="Repeatmasker .out file")
+    parser.add_argument("-o", default=None, help="Output filename")
     args = parser.parse_args()
 
     NG = NgramList(args.i)
-    NG.grow_ngrams()
-    NG.clean_overlapping()
-    NG.printNgram(3)
+    NG.grow_ngrams(10)
+    #  NG.clean_overlapping()
+    NG.writeNgrams(args.o, 3, 4)
