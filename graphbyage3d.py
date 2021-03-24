3df85d4a2c108fb58631eebeae45834c7524b3f8
Cristian <cche@users.noreply.github.com>
2021-03-24 12:15:04 +0100

    Cleaned up old code.

--- graphbyage3d.py
+++ graphbyage3d.py
@@ -29,8 +29,6 @@ if __name__ == "__main__":
     parser.add_argument("-o", help="Filename for graph")
     args = parser.parse_args()
 
-    # plt.rcParams.update({"font.size": 10})
-    # plt.subplots_adjust(wspace=0.4, hspace=0.4)
     size = args.s.split("x")
     W = int(size[0])
     H = int(size[1])
@@ -44,39 +42,31 @@ if __name__ == "__main__":
     df = tdf[tdf.Score.str.startswith("###")]
     # print(df.head())
 
-    # df["%_of_Ref%"] = df.iloc["%_of_Ref"] * 100
     fig = plt.figure(figsize=(W, H))
     plt.title("% similarity for " + args.f)
     ax = fig.add_subplot(111, projection="3d")
     if args.f != "":
         dd = df[df["Element"] == args.f]
-        # dd["%_Div"].hist(bins=int(args.b), by=dd["Element"])
         hist, xedges, yedges = np.histogram2d(
             dd["%_Div"],
             dd["%_of_Ref"] * 100,
             bins=[20, int(args.b)],
             range=[[0, 20], [0, 100]],
         )
-        # Construct arrays for the anchor positions of the 16 bars.
+        # Construct arrays for the anchor positions.
         xpos, ypos = np.meshgrid(xedges[:-1] + 0.25, yedges[:-1] + 0.25, indexing="ij")
         xpos = xpos.ravel()
         ypos = ypos.ravel()
         zpos = 0
 
-        # Construct arrays with the dimensions for the 16 bars.
+        # Construct arrays with the dimensions for the bars.
         dx = dy = 0.5 * np.ones_like(zpos)
         dz = hist.ravel()
 
         ax.view_init(elev=None, azim=-25)
         ax.bar3d(xpos, ypos, zpos, dx, dy, dz, zsort="average", shade=True)
-        # plt.subplots_adjust(
-        # top=0.92, bottom=0.05, left=0.10, right=0.95, hspace=0.7, wspace=0.45
-        # )
-        # fig = plt.gcf()
-        # fig.set_size_inches(W, H)
     else:
         dd = df
-        # plt.figure()
         dd["%_Div"].hist(bins=int(args.b), by=dd["Family"])
 
     plt.savefig(args.o, dpi=300)
