30f91376ef465e08400f7155fb54ad17dc21b32d
Cristian <cche@users.noreply.github.com>
2023-01-26 16:41:21 +0100

    Formatted code

--- getregions.py
+++ getregions.py
@@ -19,11 +19,18 @@ def bed_from_line(line, cols, regionchrom, regionstart, regionstop):
     else:
         name = score = strand = -1
 
-    fields = line.strip().split('\t')
+    fields = line.strip().split("\t")
     if name >= 0:
         return "\t".join(
-            [fields[chrom], str(regionstart), str(regionstop),
-                fields[name], fields[score], fields[strand]])
+            [
+                fields[chrom],
+                str(regionstart),
+                str(regionstop),
+                fields[name],
+                fields[score],
+                fields[strand],
+            ]
+        )
     else:
         return "\t".join([fields[chrom], str(regionstart), str(regionstop)])
 
@@ -52,26 +59,35 @@ def get_regions_2_bed(infile: str, columns, col: int = 0, threshold: int = 0):
     else:
         col -= 1
 
-    regionchrom = ''
+    regionchrom = ""
     regionstart = -1
     regionstop = 0
     inregion = False
     with open(infile) as fi:
         for line in fi:
-            fields = line.strip().split('\t')
+            fields = line.strip().split("\t")
             if threshold == 0:
                 if name >= 0:
-                    result.append("\t".join(
-                        [fields[chrom], str(start), str(end),
-                            fields[name], fields[score], fields[strand]]))
-                else:
                     result.append(
-                        "\t".join([fields[chrom], str(start), str(end)]))
+                        "\t".join(
+                            [
+                                fields[chrom],
+                                str(start),
+                                str(end),
+                                fields[name],
+                                fields[score],
+                                fields[strand],
+                            ]
+                        )
+                    )
+                else:
+                    result.append("\t".join([fields[chrom], str(start), str(end)]))
             else:
                 if fields[chrom] != regionchrom:
                     if inregion:
                         result.append(
-                            "\t".join([regionchrom, str(regionstart), str(regionstop)]))
+                            "\t".join([regionchrom, str(regionstart), str(regionstop)])
+                        )
                     regionchrom = fields[chrom]
                     regionstart = -1
                     regionstop = 0
@@ -83,7 +99,8 @@ def get_regions_2_bed(infile: str, columns, col: int = 0, threshold: int = 0):
 
                 if int(fields[col]) <= threshold and inregion:
                     result.append(
-                        "\t".join([regionchrom, str(regionstart), str(regionstop)]))
+                        "\t".join([regionchrom, str(regionstart), str(regionstop)])
+                    )
                     inregion = False
                     regionstart = -1
                     regionstop = 0
@@ -92,8 +109,7 @@ def get_regions_2_bed(infile: str, columns, col: int = 0, threshold: int = 0):
                     regionstop = fields[end]
 
         if regionchrom != "" and regionstart != -1 and regionstop != 0:
-            result.append(
-                "\t".join([regionchrom, str(regionstart), str(regionstop)]))
+            result.append("\t".join([regionchrom, str(regionstart), str(regionstop)]))
     return result
 
 
@@ -101,18 +117,21 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(
-        description="Parse tsv file and extract contiguous non zero regions in BED format")
+        description="Parse tsv file and extract contiguous non zero regions in BED format"
+    )
     parser.add_argument(
-        "-i", help="TAB separated file with at least a Chrom, star and end column")
+        "-i", help="TAB separated file with at least a Chrom, star and end column"
+    )
     parser.add_argument("-c", type=int, help="Column number to filter")
-    parser.add_argument("-t", type=int, default=0,
-                        help="Threshold to consider region")
+    parser.add_argument("-t", type=int, default=0, help="Threshold to consider region")
     parser.add_argument(
-        "-p", help="Column numbers describing position in the form 'chrom,start,end[,name,score,strand]' no spaces and comma delimited")
+        "-p",
+        help="Column numbers describing position in the form 'chrom,start,end[,name,score,strand]' no spaces and comma delimited",
+    )
     parser.add_argument("-o", help="Output in bed format")
     options = parser.parse_args()
 
     regions = get_regions_2_bed(options.i, options.p, options.c, options.t)
-    with open(options.o, 'w') as fo:
+    with open(options.o, "w") as fo:
         for region in regions:
-            fo.write(region + '\n')
+            fo.write(region + "\n")
